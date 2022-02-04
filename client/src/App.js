48e49f255b38a7879ca38d1b086359b35f8bd04b
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-02-04 09:30:13 +0100

    working on the front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,29 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import MessageContainer from './components/MessageContainer';
+
 const App = () => {
-  return <div>App</div>;
+  const [messages, setMessages] = useState([]);
+  const fetchData = async () => {
+    try {
+      const { data } = await axios('/api/v1/comments');
+      setMessages(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <main>
+      {messages.map((message) => {
+        return <MessageContainer key={message._id} {...message} />;
+      })}
+    </main>
+  );
 };
 
 export default App;
